f7395cad31dc60294f40c96a126a1a3afba42966
Laura <laurainnz2@gmail.com>
2025-04-01 22:15:03 +1300

    4.1

--- src/main/java/cn/tedu/shoot/World.java
+++ src/main/java/cn/tedu/shoot/World.java
@@ -1,6 +1,17 @@
 package cn.tedu.shoot;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class World {
     public static final int WIDTH = 400;
     public static final int HEIGHT = 700;
+
+    private Sky sky = new Sky();
+    private Hero hero = new Hero();
+    private FlyingObject[] enemies = {};//Contains Airplane,BigAirplane,Bee
+    private Bullet[] bullets = {};
+
+    public static void main(String[] args){
+
+    }
 }
