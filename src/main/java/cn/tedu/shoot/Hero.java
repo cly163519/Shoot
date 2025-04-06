07c86fe606a6fd5b97bb17d6dff1c08230dabea6
Laura <laurainnz2@gmail.com>
2025-04-06 14:59:25 +1200

    4.5

--- src/main/java/cn/tedu/shoot/Hero.java
+++ src/main/java/cn/tedu/shoot/Hero.java
@@ -16,12 +16,17 @@ public class Hero extends FlyingObject{
     }
     // The hero aircraft fires bullets, i.e., creates bullet objects
     public Bullet[] shoot(){
+        int xStep = this.width/4;//1/4 of Hero's width
+        int yStep = 20;//20 is a fixed number
         if(fire>0){//Double fire
             Bullet[] bs = new Bullet[2];//Creates an array with 2 bullets
-            bs[0] = new Bullet[];
+            bs[0] = new Bullet(this.x+1*xStep,this.y-yStep);//x:Hero's x coordinate+1/4 of Hero's x coordinate
+            bs[1] = new Bullet(this.x+3*xStep,this.y-yStep);
+            fire -= 2;//Firing a double fire reduces the fire value by 2
             return bs;
         }else{ //Single fire
             Bullet[] bs = new Bullet[1];//Creates an array with 1 bullets
+            bs[0] = new Bullet(this.x+2*xStep,this.y-yStep);
             return bs;
         }
     }
